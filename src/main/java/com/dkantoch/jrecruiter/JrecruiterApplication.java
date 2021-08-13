dd1d74974fc36f18b88cc6628ff394875dd7b468
longdavid2k17 <kantochdawid98@gmail.com>
2021-08-13 21:08:20 +0200

    Initial commit

---
+++ src/main/java/com/dkantoch/jrecruiter/JrecruiterApplication.java
@@ -0,0 +1,13 @@
+package com.dkantoch.jrecruiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrecruiterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JrecruiterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dkantoch/jrecruiter/JrecruiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkantoch.jrecruiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrecruiterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
